9c269a213162025f9a640623971080aba4a5ad70
AleksaHrevtosva <hrevtosva@yahoo.com>
2020-06-09 16:38:12 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import message from "./message";
+import arrow from "./arrow";
+import "./a.css";
+import "./b.css";
+import listItemsTemplate from "./list-items.hbs";
+
+console.log(listItemsTemplate);
+console.log(message);
+console.log(arrow(5, 10));
