b1d6aa327af93a98eb17455ccd9147d93407c9fc
Tima0001 <microsoft.timo1@gmail.com>
2020-06-12 19:54:48 +0300

    WebPack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import message from "./message";
 import arrow from "./arrow";
 import "./styles.css";
+import "./index.html";
+import Counter from "./plugins/counter";
 import "./b.css";
 import listItemsTemplate from "./list-items.hbs";
+
+new Counter ({
+    item: document.querySelector("#counter-1"),
+});
+
+new Counter ({
+    item: document.querySelector("#counter-2"),
+    initialValue: 0,
+    step: 50,
+})
 
 console.log(listItemsTemplate);
 console.log(message);
