4182f093782903519cbde69b32453e2db947d92d
Tima0001 <microsoft.timo1@gmail.com>
2020-06-28 15:46:02 +0300

    27.06

--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
 import "./css/styles.css";
-import "./js/feedback";
+// import "./js/feedback";
+
+// // Импорт массива обьектов значения которых подставляются в шаблоны
 // import posts from "./data/posts.json";
 // console.log(posts);
+
+// // Импорт шаблона отрисовки каждого елемента
 // import item from "./templates/item.hbs";
 // console.log(item);
-// const postList = document.querySelector(".post-feed");
-// const postItem = item(posts);
-// console.log(postItem);
-// postList.insertAdjacentHTML("beforeend", postItem);
-// import Counter from "./plugins/counter";
-// import * as basicLightbox from "basiclightbox";
+// // Импорт шаблона отрисовки списка елементов
+// import itemsList from "./templates/itemsList.hbs";
+// console.log(itemsList);
 
-// import posts from "./data/posts.json";
-// console.log(posts);
-
-// import listItemsTemplate from "./templates/item.hbs";
-// console.log(listItemsTemplate);
-
-// получаем доступ с тегу ul, в который будем встраивать сгенерированные элементы
+// // Получаем доступ к тегу <ul>, внутри которого будем вставлять елемент из шаблона
 // const refs = {
 //     postFeed: document.querySelector(".post-feed"),
 // };
-// console.log(refs);
-// // 1 Создаем разметку из шаблона списка
-// const markup = listItemsTemplate(posts);
-// // console.log(markup);
-// refs.postFeed.insertAdjacentHTML("beforeend", markup);
+
+// // Создаём елемент путём передачи массива обьектов из json в шаблон елемента
+// const postItem = item(posts);
+// refs.postFeed.insertAdjacentHTML("afterbegin", postItem);
+
+// // Отрисовка всего списка
+// function displayPosts(posts) {
+//     const onePost = posts.map((post) => {
+//         itemsList(post);
+//     })
+//     refs.postFeed.insertAdjacentHTML("beforeend", onePost);
+// }
+// // displayPosts(posts);
+
+///// TIMER /////
+// получаем доступ к дом-элементам таймера
+
+const timerRefs = {
+  clockFace: document.querySelector(".clockface"),
+  startBtn: document.querySelector(`button[data-action = "start"]`),
+  stopBtn: document.querySelector(`button[data-action = "stop"]`),
+};
+console.log(timerRefs);
+
+const Timer = {
+  start() {
+    //сохраняем момент времени нажатия на кнопки старт в переменную
+    const startTime = Date.now();
+    console.log(startTime);
+    this.timerId = setInterval(()=>{
+        const curentTime = Date.now();
+        this.deltaTime = curentTime-startTime;
+    })
+  },
+  stop() {},
+};
